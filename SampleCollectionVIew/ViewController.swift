33b37e08bdf9430dece268ba940b5d0f131ef03e
unochanel <shanksryouop@gmail.com>
2018-10-17 16:37:39 +0900

    Initial Commit

---
+++ SampleCollectionVIew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleCollectionVIew
+//
+//  Created by 宇野 凌平 on 2018/10/17.
+//  Copyright © 2018年 宇野 凌平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
