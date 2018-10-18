e40db1971b2358cc9830838723bb0a6b542c770f
unochanel <shanksryouop@gmail.com>
2018-10-18 10:18:51 +0900

    origin develop

--- SampleCollectionVIew/ViewController.swift
+++ SampleCollectionVIew/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var tagList = ["眠い", "食事", "残業", "仕事", "睡眠", "勉強","７文字のタグだ", "８文字のタグです"]
+    var tagList = ["タグ作成", "眠い", "食事", "残業", "仕事", "睡眠", "勉強","７文字のタグだ", "８文字のタグです", "７文字のタグだ"]
 
     @IBOutlet weak var collectionView: UICollectionView!
 
@@ -25,8 +25,9 @@ extension ViewController {
         collectionView.delegate = self
         configureCell()
     }
+
     private func configureCell() {
-        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.reuseIdentifier)
+        collectionView.register(UINib(nibName: "Cell", bundle: nil), forCellWithReuseIdentifier: Cell.reuseIdentifier)
     }
 }
 
@@ -36,7 +37,7 @@ extension ViewController: UICollectionViewDataSource {
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.reuseIdentifier, for: indexPath) as! CollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Cell.reuseIdentifier, for: indexPath) as! Cell
         cell.configureCell(item: tagList[indexPath.row])
         return cell
     }
@@ -45,36 +46,11 @@ extension ViewController: UICollectionViewDataSource {
 extension ViewController:  UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let label = UILabel()
+        let view = UIView()
         label.text = tagList[indexPath.row]
         label.sizeToFit()
-        let labelSize = CGSize(width: label.frame.size.width, height: label.frame.size.height)
-        return labelSize
-    }
-
-}
-
-extension ViewController {
-    class CollectionViewCell: UICollectionViewCell {
-        static let reuseIdentifier = "CollectionViewCell"
-        private let label = UILabel()
-
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-            setup()
-        }
-
-        required init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-            setup()
-        }
-
-        private func setup() {
-            addSubview(label)
-        }
-
-        func configureCell(item: String) {
-            label.text = item
-            label.sizeToFit()
-        }
+        view.frame.size = CGSize(width: label.frame.size.width + 24, height: label.frame.size.height + 20)
+        let cellSize = CGSize(width: view.frame.size.width, height: view.frame.size.height)
+        return cellSize
     }
 }
