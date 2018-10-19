ca798d81d120cb6387d32064c7e4db72f0f2fec2
unochanel <shanksryouop@gmail.com>
2018-10-19 20:41:10 +0900

    create View

---
+++ SampleCollectionVIew/ViewController/CreateTagViewController/TagViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TagViewCell.swift
+//  SampleCollectionVIew
+//
+//  Created by 宇野 凌平 on 2018/10/19.
+//  Copyright © 2018年 宇野 凌平. All rights reserved.
+//
+
+import UIKit
+
+class TagViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
