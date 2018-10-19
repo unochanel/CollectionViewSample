51572b573b68d015ea130a640edd27c3003e9dda
unochanel <shanksryouop@gmail.com>
2018-10-19 14:17:43 +0900

    create viewController

---
+++ SampleCollectionVIew/ViewController/CreateTagViewController/CreateTagViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CreateTagViewController.swift
+//  SampleCollectionVIew
+//
+//  Created by 宇野 凌平 on 2018/10/19.
+//  Copyright © 2018年 宇野 凌平. All rights reserved.
+//
+
+import UIKit
+
+class CreateTagViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
